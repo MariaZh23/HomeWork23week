2767a84813db1fa56064bdb30675445b33426988
MariaZh23 <mzh083@gmail.com>
2023-12-24 21:50:38 +0300

    добавлена возможность выбирать тариф нажатием на него

--- src/Components/Tariffcard/Tariffcard.jsx
+++ src/Components/Tariffcard/Tariffcard.jsx
@@ -1,8 +1,14 @@
 import './Tariffcard.scss'
+import React, { useState } from 'react';
 
-export default function Tariffcard({ name, price, speed, active, color }) {
+export default function Tariffcard({ name, price, speed, color }) {
+  const [cardSelected, setCardSelected] = useState(false);
+  const cardClick = () => {
+    setCardSelected(!cardSelected);
+  };
+
   return (
-    <div className={`tariff-card ${active}`}>
+    <div className={`tariff-card ${cardSelected ? 'selected' : ''}`} onClick={cardClick}>
       <p className={`tariff-card-name ${color}`}>Безлимитный {name}</p>
       <div className={`tariff-card-price  ${color}`}>
         <p className="price-currency">руб</p>
