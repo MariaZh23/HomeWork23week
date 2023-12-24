da38e39ce13fd16f2b4757686830f929ea590d90
MariaZh23 <mzh083@gmail.com>
2023-12-24 21:50:38 +0300

    добавktyf возможность выбирать тариф нажатием на него

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,10 +3,10 @@ import '../App/App.scss'
 
 function App() {
   const tariffs = [
-    { name: 300, price: 300, speed: 10, active: 'false', color: "darkblue" },
-    { name: 450, price: 450, speed: 50, active: 'false', color: "darkgreen" },
-    { name: 500, price: 500, speed: 100, active:'true', color: "darkred" },
-    { name: 1000, price: 1000, speed: 200, active: 'false', color: "black" },
+    { name: 300, price: 300, speed: 10, color: "darkblue" },
+    { name: 450, price: 450, speed: 50,  color: "darkgreen" },
+    { name: 500, price: 500, speed: 100,  color: "darkred" },
+    { name: 1000, price: 1000, speed: 200,  color: "black" },
   ];
   return (
     <>
@@ -17,7 +17,6 @@ function App() {
           name={tariff.name}
           price={tariff.price}
           speed={tariff.speed}
-          active={tariff.active}
           color={tariff.color}
         />
       ))}
